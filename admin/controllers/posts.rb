a7089e26cf5ebf5abec8142752913133a59b35c2
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2012-06-12 13:09:51 +0900

    投稿にアカウントを紐付けるのにコンボボックスでの動作確認

--- admin/controllers/posts.rb
+++ admin/controllers/posts.rb
@@ -2,16 +2,21 @@ Admin.controllers :posts do
 
   get :index do
     @posts = Post.all
+    @accounts = Account.all
     render 'posts/index'
   end
 
   get :new do
     @post = Post.new
+    @accounts = Account.all
     render 'posts/new'
   end
 
   post :create do
     @post = Post.new(params[:post])
+    p params[:post]
+    @accounts = Account.all
+
     if @post.save
       flash[:notice] = 'Post was successfully created.'
       redirect url(:posts, :edit, :id => @post.id)
@@ -22,11 +27,14 @@ Admin.controllers :posts do
 
   get :edit, :with => :id do
     @post = Post.find(params[:id])
+    @select_account_id = @post.account_id
+    @accounts = Account.all
     render 'posts/edit'
   end
 
   put :update, :with => :id do
     @post = Post.find(params[:id])
+    p params[:post]
     if @post.update_attributes(params[:post])
       flash[:notice] = 'Post was successfully updated.'
       redirect url(:posts, :edit, :id => @post.id)
