4770c18315f5a0f03e09382c30b873413ea40bad
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2012-06-20 20:14:23 +0900

    aaa

--- models/post.rb~
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :account
-  validates_presence_of :title
-  validates_presence_of :body
-end
--- spec/factories/authors.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :author do
-    sequence(:name) {|n| "author_#{n}" }
-    sequence(:name_kana) {|n| "author_kana_#{n}" }
-  end
-end
