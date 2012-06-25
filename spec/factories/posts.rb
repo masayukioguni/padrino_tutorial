709e36571bb768109e221a694bb73ac0210106e3
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2012-06-25 18:32:17 +0900

    add .autotest .rspec

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,5 @@
 FactoryGirl.define do
   factory :post do
-    #sequence(:title) {|n| "title_#{n}" }
-    #sequence(:body) {|n| "body_#{n}" }
-    #sequence(:account_id)
     title { Faker::Name.name }
     body { Faker::Name.name }
     sequence(:account_id)
